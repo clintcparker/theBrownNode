b6714e5b857f668ea304d7dd4c8a06db08a409c7
clintcparker <clintcparker@gmail.com>
2012-02-09 02:03:12 +0000

    helloo

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 http.createServer(function (request, response) {
   response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('Hellooooooooo World\n');
+  response.end('Helloooooooooo World\n');
 }).listen(process.env.C9_PORT || process.env.PORT || 3000);
 
 console.log('Server running at http://127.0.0.1:8124/');
