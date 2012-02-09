d3a7aa9bbea0e7c3a0ffd16e64bc075cf4f2783a
clintcparker <clintcparker@gmail.com>
2012-02-09 02:25:45 +0000

    added that bit aabout express

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send('Hello World!, i am using express');
 });
 
 var port = process.env.C9_PORT || process.env.PORT || 3000;
